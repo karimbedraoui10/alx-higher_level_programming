7afd593978b0f40f1c8bf8d798b018c5aaeec016
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-11-29 09:00:44 -0800

    add 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(1,90):
     if n == 89:
-        print(f"{n:02d}")
+        print("{:02d}".format(n))
     else:
-        print(f"{n:02d}, ",end="")
+        print("{:02d}, ".format(n), end="")
