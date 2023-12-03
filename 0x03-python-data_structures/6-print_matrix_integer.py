73dd05934f0c89b7d6e21c4666b9974e93350fa4
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-12-03 07:35:07 -0800

    add 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        return None
+    for raw in matrix:
+        if len(raw) == 0:
+            print()
+        for i in range(len(raw)):
+            print("{:d}".format(raw[i]), end="\n" if i == len(raw) - 1 else " ")
