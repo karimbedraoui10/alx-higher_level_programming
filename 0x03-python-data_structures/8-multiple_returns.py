97f3264408931a02356918b8800e1259af01843b
karimbedraoui10 <karimbedraoui10@gmail.com>
2023-12-05 07:50:17 -0800

    add 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    size = len(sentence)
-    if size != 0:
-        frst = sentence[0]
-        return size, frst
-    else:
-         return size, None
+    return (len(sentence), sentence[0] if len(sentence) > 0 else None)
